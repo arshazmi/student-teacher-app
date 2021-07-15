b7561553d1eb230b9dffdf534f3a0f2cd59006e1
dawti <dawtiej@gmail.com>
2021-07-15 13:49:50 +0530

    first commit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 3000;
 app.use(express.urlencoded({extended: true}));
 app.use(express.json()) // To parse the incoming requests with JSON payloads
 app.use(express.static('public'))
@@ -8,12 +8,45 @@ app.set('view engine', 'ejs');
 var pgp = require('pg-promise')( /* options */ )
 var db = pgp('postgres://postgres:password@localhost:5432/tablename');
 
-
-
 app.get('/', (req, res) => {
-    res.render('teacher')
+    res.render('teacher');
 })
 
+app.get('/teacher/edit/:id', async function(req,res){
+    await db.any('SELECT id,name,course FROM register WHERE id=${id}',
+       {id:req.params.id} 
+    ).then(data=>{
+        console.log("Data from db:",data);
+        res.status(200).json(data);
+        });
+
+});
+
+app.post('/teacher/edit/:id', async function(req,res){
+    console.log('Params::',req.params);
+    await db.none('UPDATE teacher SET name=${fn},  course=${sub} WHERE id=${id}',
+    {
+      fn:req.body.fn,
+      sub:req.body.sub,
+      id:req.params.id      
+    });
+    var data = {
+        'status': "Valid",
+        'statuscode': "5002", 
+    };
+    res.status(200).json(data);
+});
+
+app.get('/teacher/delete/:id', async function(req,res){
+    await db.none('DELETE  FROM teacher where id=${id}',{
+        id:req.params.id
+    });
+    var data = {
+        'status': "Valid",
+        'statuscode': "5004", 
+    };
+     res.status(200).json(data);
+});
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
