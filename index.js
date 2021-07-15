bc323ba5cf117bba7df144aadc076b9f6a235f36
Arsheena <aazmi600@gmail.com>
2021-07-15 13:48:04 +0530

    teacher create and read api,changed teacher page images and style

--- index.js
+++ index.js
@@ -6,14 +6,50 @@ app.use(express.json()) // To parse the incoming requests with JSON payloads
 app.use(express.static('public'))
 app.set('view engine', 'ejs');
 var pgp = require('pg-promise')( /* options */ )
-var db = pgp('postgres://postgres:password@localhost:5432/tablename');
+var db = pgp('postgres://postgres:root@localhost:5432/student-teacher');
 
 
 
 app.get('/', (req, res) => {
     res.render('teacher')
 })
+//techer read operation
+app.get('/teacher/select', async (req, res) => {
+    await db.any('select * from teacher').then(datan => {
+        console.log(data);
+        var data = {
+            'status': "Valid",
+            "data": datan
+        };
+        res.status(200).json(data);
+    }).catch(error => {
+        console.log(error);
+        var data = {
+            'status': "invalid",
+            "msg": error
+        };
+        res.status(200).json(data);
+    });
+})
+//teacher create operation
+app.post('/teacher/select/post', async (req, res) => {
+    console.log(req.params);
+    console.log(req.body);
+    if (req.body.id === '') {
+        await db.none('INSERT INTO teacher(name,course) VALUES(  ${name},${course})', { 
+            name: req.body.name,
+            course: req.body.course
+        });
 
+    } else {
+        await db.none('update teacher set  name=${name}, password=${course} where id = ${id}', {
+            name: req.body.name,
+            course: req.body.course,
+            id: req.body.id
+        })
+    }
+
+})
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
