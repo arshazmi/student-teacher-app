789c6303895b702c7a3aefb96d0d240b79c34497
corrine3297 <corrine.elizabeth.rodrigues04@gmail.com>
2021-07-16 14:35:02 +0530

    student feedback added

--- index.js
+++ index.js
@@ -118,7 +118,83 @@ app.post('/api/select/post', async (req, res) => {
     res.status(200).json(data);
 
 })
+app.get('/studfeedbck',(req,res)=>{
+    res.render('studfeedbck')
+})
+
+//----------------------- feedback----------------------------------
+app.post('/api/feedback/post', async (req, res) => {
+    console.log(req.params);
+    console.log(req.body);
+    if (req.body.id === '') {
+        await db.none('INSERT INTO feedback (teacher,feed) VALUES(${teacher},${feed})', { 
+            teacher: req.body.teacher,
+            feed: req.body.feed
+           
+           
+        });
+
+    } else {
+        await db.none('update feedback set  teacher=${teacher},feed=${feed} where id = ${id}', {
+            teacher: req.body.teacher,
+            feed: req.body.feed,
+            id: req.body.id
+        })
+    }
+
+    var data = {
+        'status': "Valid",
+        'status-code': "5001",
+    };
+    res.status(200).json(data);
+
+})
+//----------------------- feedback- select---------------------------------
+app.get('/api/selectapi', async (req, res) => {
+    console.log(req.params.id);
+    await db.any('select * from  feedback ').then(datan => {
+        var data = {
+            'status': "Valid",
+            'status-code': "5001",
+            "data": datan
+        };
+        res.status(200).json(data);
+    }).catch(error => {
+        console.log(error);
+        var data = {
+            'status': "invalid",
+            'status-code': "5000",
+            "msg": error
+        };
+        res.status(200).json(data);
+
+    });
+})
+//----------------------- feedback- select edit---------------------------------
+app.get('/api/selectapi/edit/:id', async (req, res) => {
+    console.log(req.params.id);
+    await db.any('select id,teacher,feed from feedback where id='+ req.params.id ).then(datan => {
+        var data = {
+            'status': "Valid",
+            'status-code': "5001",
+            "data": datan
+        };
+        res.status(200).json(data);
+    }).catch(error => {
+        console.log(error);
+        var data = {
+            'status': "invalid",
+            'status-code': "5000",
+            "msg": error
+        };
+        res.status(200).json(data);
+
+    });
+})
+
+
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
 })
+
